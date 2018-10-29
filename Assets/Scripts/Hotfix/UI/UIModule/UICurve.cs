3d0c3d1eeb9a05f25ed7dd86a6a235f099d700f5
a395338563 <395338563@qq.com>
2018-10-29 15:49:06 +0800

    稍微写了下开发指南。 封装了一下那个箭头

--- Assets/Scripts/Hotfix/UI/UIModule/UICurve.cs
+++ Assets/Scripts/Hotfix/UI/UIModule/UICurve.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FairyGUI;
+using THClimbTower;
+using UnityEngine;
 
 namespace Hotfix.UIModule
 {
     public class UICurve
     {
+        GComponent main;
+        public UICurve(GComponent component)
+        {
+            main = component;
+        }
+        public void ChangeShape(Vector2 start,Vector2 end,Vector2 control)
+        {
+            Vector2 Last = start;
+            for (int i = 1; i < 20; i++)
+            {
+                Vector2 pos = BezierHelper.Quadratic(i / 20f, start, end, control);
+                Vector2 delta = pos - Last;
+                delta.y *= -1;
+                if (i > 0)
+                {
+                    main.GetChild($"n{i - 1}").rotation = 360 - Vector2.SignedAngle(Vector2.up, delta);
+                }
+                if (i == 19)
+                {
+                    main.GetChild($"n{i-1}").rotation = 360 - Vector2.SignedAngle(Vector2.up, delta);
+                    main.GetChild("Head").rotation = 360 - Vector2.SignedAngle(Vector2.up, delta);
+                }
+                main.GetChild($"n{i-1}").xy = pos;
+                Last = pos;
+            }
+            main.GetChild("Head").xy = end;
+        }
     }
 }
--- Assets/Scripts/Hotfix/UI/View/BattleView.cs
+++ Assets/Scripts/Hotfix/UI/View/BattleView.cs
@@ -22,9 +22,11 @@ namespace Hotfix.View
 
         GList RelicList;
 
-        GComponent Hand,Arrow;
+        GComponent Hand;
 
         UICharactor player;
+
+        UICurve curve;
 
         /// <summary>
         /// 为true时，表示玩家选中了一张牌，准备打出
@@ -35,7 +37,7 @@ namespace Hotfix.View
         {
             Hand = MainView.GetChild("n60").asCom;
             RelicList = MainView.GetChild("n53").asList;
-            Arrow = MainView.GetChild("n61").asCom;
+            curve = new UICurve(MainView.GetChild("n61").asCom);
 
             THClimbTower.EventSystem.Instance.RunEvent(THClimbTower.EventType.TestBattle);
             //THClimbTower.Game.EventSystem.RunEvent(THClimbTower.EventType.GameStart);
@@ -152,47 +154,8 @@ namespace Hotfix.View
             //Debug.Log(screenpos);
 
             Vector2 start = new Vector2(960, 780);
-            Vector2 Last = start;
             Vector2 contorl = new Vector2(start.x - (screenpos.x - start.x) / 4, screenpos.y + (screenpos.y - start.y) / 2);
-            for (int i = 0; i < 19; i++)
-            {
-                Vector2 pos = BezierHelper.Quadratic(i / 20f, start, screenpos, contorl);
-                //float x = BackIn(screenpos.x-start.x, ((float)i) / 19);
-                //float y = BackOut(screenpos.y-start.y, ((float)i) / 19);
-                Vector2 delta = pos - Last;
-                delta.y *= -1;
-                if (i > 0)
-                {
-                    Arrow.GetChild($"n{i - 1}").rotation = 360-Vector2.SignedAngle(Vector2.up, delta);
-                }
-                if (i == 18)
-                {
-                    Arrow.GetChild($"n{i}").rotation = 360 - Vector2.SignedAngle(Vector2.up, delta);
-                    Arrow.GetChild("Head").rotation= 360 - Vector2.SignedAngle(Vector2.up, delta);
-                }
-                Arrow.GetChild($"n{i}").xy = pos;
-                Last = pos;
-            }
-            Arrow.GetChild("Head").xy = screenpos;
-            Arrow.AddChild(Arrow.GetChild("Head"));
-        }
-        float BackOut(float value, float time)
-        {
-            time -= 1.0f;
-            return value * (time * time * (2.70158f * time + 1.70158f) + 1.0f);
-        }
-        float BackIn(float value,  float time)
-        {
-            return value * time * time * (2.70158f * time - 1.70158f);
-        }
-        Vector2 quadratic(Vector2 output, float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 tmp)
-        {
-            float dt = 1.0F - t;
-            output = p0;
-            output *= dt * dt;
-            output += p1 * 2 * dt * t;
-            output += p2 * t * t;
-            return output;
+            curve.ChangeShape(start, screenpos, contorl);
         }
     }
 }
